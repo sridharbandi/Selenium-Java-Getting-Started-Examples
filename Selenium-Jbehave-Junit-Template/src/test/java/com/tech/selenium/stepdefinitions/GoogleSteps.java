4e30e65e8734d9a43e6a8088385978821555a3de
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-02-12 06:02:36 +0000

    Updated

---
+++ Selenium-Jbehave-Junit-Template/src/test/java/com/tech/selenium/stepdefinitions/GoogleSteps.java
@@ -0,0 +1,48 @@
+package com.tech.selenium.stepdefinitions;
+
+import com.tech.selenium.driverutil.DriverFactory;
+import com.tech.selenium.pageobjects.GoogleSearchPage;
+import com.tech.selenium.pageobjects.SearchResultsPage;
+import org.jbehave.core.annotations.*;
+import org.jbehave.core.steps.Steps;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+
+
+/**
+ * Created by Sridhar Bandi on 29/01/19.
+ */
+public class GoogleSteps extends Steps {
+
+    private WebDriver driver;
+    GoogleSearchPage googleSearchPage;
+    SearchResultsPage searchResultsPage;
+
+    @BeforeScenario
+    public void setUp(){
+        driver = DriverFactory.getBrowser();
+        googleSearchPage = new GoogleSearchPage(driver);
+        searchResultsPage = new SearchResultsPage(driver);
+    }
+
+    @Given("User is on Google search page")
+    public void userOnGooglePage(){
+        googleSearchPage.openURL();
+    };
+
+    @When("User searches for Selenium")
+    public void userSearches(){
+        googleSearchPage.searchFor("Selenium");
+    }
+
+    @Then("User can see Selenium results")
+    public void userSeesResults(){
+        Assert.assertTrue(searchResultsPage._lnkSelenium.isDisplayed());
+    }
+
+    @AfterScenario
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
---
+++ Selenium-Jbehave-Junit-Template/src/test/resources/stories/Google.story
@@ -0,0 +1,6 @@
+Feature: Selenium Jbehave jUnit Example
+
+  Scenario: Google search
+    Given User is on Google search page
+    When User searches for Selenium
+    Then User can see Selenium results
