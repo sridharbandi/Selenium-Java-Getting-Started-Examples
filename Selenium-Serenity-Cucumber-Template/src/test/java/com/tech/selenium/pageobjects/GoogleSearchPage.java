76f00c2632e5d417fd05f00eb174040485589c1d
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-14 20:30:54 +0100

    Pageobjects

---
+++ Selenium-Serenity-Cucumber-Template/src/test/java/com/tech/selenium/pageobjects/GoogleSearchPage.java
@@ -0,0 +1,22 @@
+package com.tech.selenium.pageobjects;
+
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
+import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.Keys;
+
+/**
+ * Created by Sridhar Bandi on 14/05/19.
+ */
+@DefaultUrl("https://www.google.com/")
+public class GoogleSearchPage extends PageObject {
+
+    @FindBy(name = "q")
+    WebElementFacade searchbox;
+
+    public void searchFor(String _searchTerm) {
+        searchbox.waitUntilVisible().sendKeys(_searchTerm + Keys.RETURN);
+    }
+
+}
---
+++ Selenium-Serenity-Cucumber-Template/src/test/java/com/tech/selenium/pageobjects/SearchResultsPage.java
@@ -0,0 +1,21 @@
+package com.tech.selenium.pageobjects;
+
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Sridhar Bandi on 14/05/19.
+ */
+public class SearchResultsPage extends PageObject {
+
+    private WebDriver _driver = null;
+
+    @FindBy(partialLinkText = "Selenium - Web Browser Automation")
+    WebElementFacade lnkSelenium;
+
+    public boolean seleniumResultPresent(){
+        return lnkSelenium.isDisplayed();
+    }
+}
