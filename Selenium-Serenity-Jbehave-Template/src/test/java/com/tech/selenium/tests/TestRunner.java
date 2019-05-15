06681b42b57901f6b279e6be36eb3b97e2b29bb0
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-15 20:59:15 +0100

    Test Runner Class

---
+++ Selenium-Serenity-Jbehave-Template/src/test/java/com/tech/selenium/tests/TestRunner.java
@@ -0,0 +1,10 @@
+package com.tech.selenium.tests;
+
+import net.serenitybdd.jbehave.SerenityStories;
+
+/**
+ * Created by Sridhar Bandi on 15/05/19.
+ */
+public class TestRunner extends SerenityStories {
+
+}
