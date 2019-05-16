0b2fa0ca538d722e6e6ca8eafd111d49fac4c9ea
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-16 21:20:55 +0100

    Selenide Cucumber Junit

---
+++ Selenium-Selenide-Cucumber-Junit-Template/src/test/java/com/tech/selenium/stepdefinitions/GoogleSteps.java
@@ -0,0 +1,39 @@
+package com.tech.selenium.stepdefinitions;
+
+import com.tech.selenium.driverutil.DriverFactory;
+import com.tech.selenium.pageobjects.GoogleSearchPage;
+import com.tech.selenium.pageobjects.SearchResultsPage;
+import cucumber.api.Scenario;
+import cucumber.api.java8.En;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Sridhar Bandi on 16/05/19.
+ */
+public class GoogleSteps implements En {
+
+    GoogleSearchPage googleSearchPage;
+    SearchResultsPage searchResultsPage;
+
+    public GoogleSteps() {
+
+        Before((Scenario scenario) -> {
+            DriverFactory.setBrowser();
+            googleSearchPage = new GoogleSearchPage();
+            searchResultsPage = new SearchResultsPage();
+        });
+
+        Given("^User is on Google search page$", () -> {
+            googleSearchPage.openURL();
+        });
+
+        When("^User searches for Selenium$", () -> {
+            googleSearchPage.searchFor("Selenium");
+        });
+
+        Then("^User can see Selenium results$", () -> {
+            Assert.assertTrue(searchResultsPage.getLnkSelenium().isDisplayed());
+        });
+    }
+}
---
+++ Selenium-Selenide-Cucumber-Junit-Template/src/test/resources/features/Google.feature
@@ -0,0 +1,6 @@
+Feature: Selenium Selenide Cucumber Junit Example
+
+  Scenario: Google search
+    Given User is on Google search page
+    When User searches for Selenium
+    Then User can see Selenium results
