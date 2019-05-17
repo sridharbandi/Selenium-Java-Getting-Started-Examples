3c284a0b99b0685cc83b482b29257eabe3b02a6a
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-17 10:05:58 +0100

    Page Objects

---
+++ Selenium-Selenide-Jbehave-Testng-Template/src/test/java/com/tech/selenium/pageobjects/SearchResultsPage.java
@@ -0,0 +1,19 @@
+package com.tech.selenium.pageobjects;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byPartialLinkText;
+import static com.codeborne.selenide.Selenide.$;
+
+/**
+ * Created by Sridhar Bandi on 17/05/19.
+ */
+public class SearchResultsPage {
+
+    private SelenideElement lnkSelenium = $(byPartialLinkText("Selenium - Web Browser Automation"));
+
+    public SelenideElement getLnkSelenium(){
+        return lnkSelenium.waitUntil(Condition.appear, 60000);
+    }
+}
