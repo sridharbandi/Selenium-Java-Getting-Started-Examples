f766c0eae69e2c4a275fa3fe7ed98010ce062ed3
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2020-05-05 19:30:10 +0200

    Updated the packages

--- Selenium-Cucumber-Testng-Template/src/test/java/com/tech/selenium/tests/TestRunner.java
+++ Selenium-Cucumber-Testng-Template/src/test/java/com/tech/selenium/tests/TestRunner.java
@@ -1,7 +1,7 @@
 package com.tech.selenium.tests;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
 /**
  * Created by Sridhar Bandi on 29/01/19.
